889963405f912cbbf1c88641e1b995657a394e87
LUO, Chun <chun.luo.2019@gmail.com>
2024-08-10 09:58:35 +0100

    Modified hangman33/hangman/milestone_2

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,1 +1,11 @@
-
+import random
+word_list = ['apple', 'pear', 'cherry', 'watermelon', 'strawberry']
+print(word_list)
+choice = random.choice
+word = choice(word_list)
+print(word)
+guess = input("Please enter a single letter.")
+if len(guess) == 1 and guess.isalpha() == True:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
